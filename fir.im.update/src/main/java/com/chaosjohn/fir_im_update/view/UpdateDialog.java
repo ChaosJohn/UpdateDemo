feacbb1827636c21967e78fad205981185f682ce
Chaos John <chaosjohn.yjh@icloud.com>
2016-09-21 10:36:39 +0800

    update dialog cannot be dismiss by back button

--- fir.im.update/src/main/java/com/chaosjohn/fir_im_update/view/UpdateDialog.java
+++ fir.im.update/src/main/java/com/chaosjohn/fir_im_update/view/UpdateDialog.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -57,5 +58,15 @@ public class UpdateDialog extends Activity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                /*&& event.getRepeatCount() == 0*/) {
+            //do something...
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
